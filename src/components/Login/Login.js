e9f60989df56aa823c7b2a9e5f47dd1a3384f2ad
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-10 12:41:23 +0600

    built signin and signup pages and created event handler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 const Login = () => {
   return (
     <section>
       <div className="login-container">
         <h1 className="login-heading">Login</h1>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="email" placeholder="Enter your email" required />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" placeholder="Enter your password" required />
+            <br />
+            <input type="submit" value="Login" className="login-btn" />
+          </div>
+        </form>
+        <p>
+          New to Ema-Jhon?
+          <Link to={"/signup"}>
+            {" "}
+            <span className="form-link">Create an account</span>
+          </Link>{" "}
+          now!
+        </p>
+        <input
+          type="submit"
+          value="Continue With Google"
+          className="google-login-btn"
+        />
       </div>
     </section>
   );
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Signup.css';
 const Signup = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [cpassword,setCPassword] = useState('');
+    const [error,setError] = useState('');
+
+    const handleEmailBLur = (e)=>{
+        setEmail(e.target.value);
+        console.log(email);
+    }
+    const handlePassBLur = (e)=>{
+        setPassword(e.target.value);
+        console.log(password);
+    }
+    const handleCPassBLur = (e)=>{
+        setCPassword(e.target.value);
+        console.log(cpassword);
+    }
+
     return (
         <div>
-            <h1 className='signup-heading'>Signup Page</h1>
+             <section>
+      <div className="login-container">
+        <h1 className="login-heading">Signup</h1>
+        <form>
+          <div className="input-group">
+          
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="email" placeholder="Enter your email" required onBlur={handleEmailBLur}/>
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" placeholder="Enter your password" required onBlur={handlePassBLur}/>
+            <br />
+            <label htmlFor="cpassword">Confirm Password</label>
+            <br />
+            <input type="password" placeholder="Confirm password" required onBlur={handleCPassBLur}/>
+            <br />
+            <input type="submit" value="Register" className="login-btn" />
+          </div>
+        </form>
+        <p>
+         Already have an account?
+          <Link to={"/login"}>
+            {" "}
+            <span className="form-link">Signin</span>
+          </Link>{" "}
+          now!
+        </p>
+        
+      </div>
+    </section>
         </div>
     );
 };
